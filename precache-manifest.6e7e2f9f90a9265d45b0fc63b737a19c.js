7a7dd947cd10c1edee192141dc7b0550b8a73ec6
DefenderKent <48011507+defender1995@users.noreply.github.com>
2020-05-14 19:41:12 +0700

    Updates

---
+++ precache-manifest.6e7e2f9f90a9265d45b0fc63b737a19c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0303aaf97ed20feb5cecd9535d83750c",
+    "url": "/Posts/index.html"
+  },
+  {
+    "revision": "f4b95ea49a981450c507",
+    "url": "/Posts/static/js/2.2bf3fc29.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/Posts/static/js/2.2bf3fc29.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e56a44e86074b904b24",
+    "url": "/Posts/static/js/main.29359db5.chunk.js"
+  },
+  {
+    "revision": "a5471d526bb5f1435509",
+    "url": "/Posts/static/js/runtime-main.22de6bc0.js"
+  }
+]);
